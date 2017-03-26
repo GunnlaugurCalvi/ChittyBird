cfad1fe9ba3ba9b2ab7c71d7a526fc8e507eb589
Axel Björnsson <axelbjornsson@gmail.com>
2017-03-26 21:04:30 +0000

    pípur líta vel út en hreyfa sig ekkkiiiii wtf

--- app/scripts/pipes.js
+++ app/scripts/pipes.js
@@ -1,35 +1,38 @@
 window.Pipes = (function () {
     'use strict';
 
-
-    var pipeId = 0;
-
+    var pipeid = 0;
+    var gapHeight = 40;
 
     var Pipes = function (el, game) {
         this.el = el;
         this.game = game;
     };
+    Pipes.prototype.spawnPipe = function() {
+        pipeid++;
+        var pipetopHeight = this.el.height() - 200;
+        var pipebottomHeight = $('.GameCanvas').height();
+        var pipe = '<div class="pipe" pipe-id="' + pipeid + '"><div style="height: ' + pipetopHeight + 'px" class="pipetop"></div><div style="height:' + pipebottomHeight + 'px" class="pipebottom"></div></div>';
+        this.el.append(pipe);
+    }
 
 
-	Pipes.prototype.reset = function() {
-        // WOW
-	};
 
-
-
-    // function spawnPipe() {
-    //     pipeId++;
-    //     pipeTopHeight = Math.floor(Math.random() * ($window.height() - 250)) + 50;
-    //     pipeBottomHeight = $window.height() - (pipeTopHeight + gapHeight);
-    //     pipe = '<div class="pipe" pipe-id="' + pipeId + '"><div style="height: ' + pipeTopHeight + 'px" class="topHalf"></div><div style="height:' + pipeBottomHeight + 'px" class="bottomHalf"></div></div>';
-    //     $window.append(pipe);
-    // }
+    Pipes.prototype.deletePipe = function() {
+        $('.window .pipe').first().remove();
+    }
 
 
     Pipes.prototype.onFrame = function (delta) {
-        // this.pos.y += (delta * SPEED) / 4;
 
         // // Update UI
+        this.spawnPipe();
+        this.el.each(function () {
+            this.animate({
+                right: '+=160px'
+            }, 1300, 'linear');
+        });
+
         // this.el.css('transform', 'translate(' + this.pos.x + 'em, ' + this.pos.y + 'em)');
     };
 
